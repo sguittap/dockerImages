5b70fc32295d5391a80f8191348eaddf342f1e30
Shane Guittap <shaneguittap@Shanes-MacBook-Pro.local>
2019-12-09 15:34:30 -0500

    docker compose w multiple local containers

--- visitor/index.js
+++ visitor/index.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const redis = require('redis');
+const process = require('process');
 
 const app = express();
-const client = redis.createClient();
+
+const client = redis.createClient({
+    host:'redis-server',
+    port: 6379
+});
 client.set('visits', 0);
 
 app.get('/', (req, res)=>{
    client.get('visits',(err, visits) => {
-        res.send("Number of visits" + visits)
-        client.set('visits', parseInt(visits) + 1)
+       process.exit(0);
+       res.send('Number of visits is ' + visits);
+       client.set('visits', parseInt(visits) + 1);
    })
 });
 
